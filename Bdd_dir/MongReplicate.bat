a4e55001b766562fe59866aab45ecf59ddf0e2b0
Coltarm <colin.treve@laposte.net>
2024-03-13 13:41:29 +0100

    ajout script connection et authentification mongodb

--- Bdd_dir/MongReplicate.bat
+++ Bdd_dir/MongReplicate.bat
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-mongod --replSet rs0 --port 27018 --dbpath mongdata/R0S1 &
-mongod --replSet rs0 --port 27019 --dbpath mongdata/R0S2 &
-mongod --replSet rs0 --port 27020 --dbpath mongdata/R0S3 &
-mongod --replSet rs0 --port 27021 --dbpath mongdata/R0S4 &
+script_directory="$(cd "$(dirname "$0")" && pwd)"
 
-mongod --port 30000 --dbpath mongdata/arb --replSet rs0 &
+# Utilise le chemin absolu pour accéder au répertoire d'intérêt
+mongod --replSet rs0 --port 27018 --dbpath $script_directory/mongdata/R0S1 &
+mongod --replSet rs0 --port 27019 --dbpath $script_directory/mongdata/R0S2 &
+mongod --replSet rs0 --port 27020 --dbpath $script_directory/mongdata/R0S3 &
+mongod --replSet rs0 --port 27021 --dbpath $script_directory/mongdata/R0S4 &
+
+mongod --port 30000 --dbpath $script_directory/mongdata/arb --replSet rs0
